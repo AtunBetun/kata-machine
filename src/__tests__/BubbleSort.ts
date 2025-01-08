a941373d25170bd64358c925a0c60da7b769aa94
AtunBetun <beto7299@gmail.com>
2025-01-07 20:43:25 -0500

    added theprimeagen stuff

--- src/__tests__/BubbleSort.ts
+++ src/__tests__/BubbleSort.ts
@@ -1,10 +1,19 @@
-import bubble_sort from "@code/BubbleSort";
+import { bubble_sort } from "@code/BubbleSort";
+import { bubble_sort_v2 } from "@code/BubbleSort";
 
-test("bubble-sort", function () {
-    const arr = [9, 3, 7, 4, 69, 420, 42];
+test("bubble-sort", function() {
+	const arr = [9, 3, 7, 4, 69, 420, 42];
 
-    debugger;
-    bubble_sort(arr);
-    expect(arr).toEqual([3, 4, 7, 9, 42, 69, 420]);
+	debugger;
+	bubble_sort(arr);
+	expect(arr).toEqual([3, 4, 7, 9, 42, 69, 420]);
+});
+
+test("bubble-sort-v2", function() {
+	const arr = [9, 3, 7, 4, 69, 420, 42];
+
+	debugger;
+	bubble_sort_v2(arr);
+	expect(arr).toEqual([3, 4, 7, 9, 42, 69, 420]);
 });
 
--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,5 +1,5 @@
-// naive
-export default function bubble_sort(arr: number[]): void {
+// naive and buggy
+export function bubble_sort(arr: number[]): void {
 	let swapped = false;
 	do {
 		swapped = false;
@@ -18,18 +18,15 @@ export default function bubble_sort(arr: number[]): void {
 
 // reducing x
 export function bubble_sort_v2(arr: number[]): void {
-	let swapped = false;
-	do {
-		swapped = false;
-		for (let x = 0; x < arr.length; ++x) {
-			// swap
-			if (arr[x] > arr[x + 1]) {
-				swapped = true;
-				let t = arr[x + 1];
-				arr[x + 1] = arr[x]
-				arr[x] = t;
+
+	for (let i = 0; i < arr.length; ++i) {
+		for (let j = 0; j < arr.length - 1 - i; ++j) {
+			if (arr[j] > arr[j + 1]) {
+				let t = arr[j + 1];
+				arr[j + 1] = arr[j]
+				arr[j] = t;
 			}
 		}
-	} while (swapped)
+	}
 
 }
